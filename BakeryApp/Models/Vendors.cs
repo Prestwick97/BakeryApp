8b3c2105f616b4ad7b7fdebd0d73449bcb480001
Student <example@example.com>
2020-03-13 12:37:21 -0700

    remove setters for Ids

--- BakeryApp/Models/Vendors.cs
+++ BakeryApp/Models/Vendors.cs
@@ -7,7 +7,7 @@ namespace BakeryApp.Models
         private static List<Vendor> _instances = new List<Vendor> {};
         public string Name {get; set;}
         public string Description {get; set;}
-        public int Id {get; set;}
+        public int Id {get;}
         public List<Order> Orders {get; set;}
     }
 }
