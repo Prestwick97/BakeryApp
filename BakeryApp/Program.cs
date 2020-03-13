4910f7b73a911c3647ae15438a2cfce841ad2b8e
Student <example@example.com>
2020-03-13 12:41:53 -0700

    populate PRogram.cs file

---
+++ BakeryApp/Program.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace BakeryApp
+{
+  public class Program
+  {
+    public static void Main(string [] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
