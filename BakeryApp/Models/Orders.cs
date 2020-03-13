cea05f6c026de96d9f4624897cdcd28f46ba050f
Student <example@example.com>
2020-03-13 12:48:25 -0700

    create Vendor constructor - fix indentation

--- BakeryApp/Models/Orders.cs
+++ BakeryApp/Models/Orders.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 
 namespace BakeryApp.Models
 {
-    public class Order
-    {
-        public string Title {get; set;}
-        public string Description {get; set;}
-        public int Price {get; set;}
-        public string Date {get; set;}
-        public int Id {get;}
-        private static List<Order> _instances = new List<Order> {};
+  public class Order
+  {
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public int Price {get; set;}
+    public string Date {get; set;}
+    public int Id {get;}
+    private static List<Order> _instances = new List<Order> {};
 
-        public Order(string title, string decription, int price, string date)
-        {
-            Title = title;
-            Description = Description;
-            Price = price;
-            Date = date;
-            _instances.Add(this);
-            Id = _instances.Count;
-        }
-    
+    public Order(string title, string decription, int price, string date)
+    {
+      Title = title;
+      Description = Description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
+  }
 }
--- BakeryApp/Models/Vendors.cs
+++ BakeryApp/Models/Vendors.cs
@@ -2,12 +2,22 @@ using System.Collections.Generic;
 
 namespace BakeryApp.Models
 {
-    public class Vendor
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public int Id {get;}
+    public List<Order> Orders {get; set;}
+
+    public Vendor(string name, string description)
     {
-        private static List<Vendor> _instances = new List<Vendor> {};
-        public string Name {get; set;}
-        public string Description {get; set;}
-        public int Id {get;}
-        public List<Order> Orders {get; set;}
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
     }
+  }
+
 }
