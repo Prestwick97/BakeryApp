7c837912aa12a836bb610ba2c7dc79baeeed7c3e
Student <example@example.com>
2020-03-13 14:41:17 -0700

    create order Controller

---
+++ BakeryApp/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryApp.Models;
+using System.Collections.Generic;
+
+namespace BakeryApp.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+       Vendor vendor = Vendor.Find(vendorId);
+       return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+
+  }
+}
