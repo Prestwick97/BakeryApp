2c1bef29e545599de556e4ae37325a646f4f5bb9
Student <example@example.com>
2020-03-13 12:36:52 -0700

    create Order class

---
+++ BakeryApp/Models/Orders.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace BakeryApp.Models
+{
+    public class Order
+    {
+        public string Title {get; set;}
+        public string Description {get; set;}
+        public int Price {get; set;}
+        public string Date {get; set;}
+        public int Id {get;}
+        private static List<Order> _instances = new List<Order> {};
+    }
+}
